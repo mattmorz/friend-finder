f121595bb6d0cce0d55ae7bf9d2e367fcfa69aa9
Matt O. Morales <idsilmat@gmail.com>
2022-04-12 15:36:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
 
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('matt')
 );
